4576ea2d08d07adf9bd7507b1e179a7c4108d386
34w]9 <42980754+CamdenSun@users.noreply.github.com>
2019-01-14 09:50:43 -0800

    test

--- signUp.js
+++ signUp.js
@@ -1,13 +1,35 @@
-const db = firebase.database()
-async function createUser(){
-	let uName = document.getElementById('suName').value;
-	let email = document.getElementById('suEmail').value;
-	let password = document.getElementById('suPass').value;
-	console.log(uName);
-	if (required(uName, "Username", "suName") && required(email, "Email", "suEmail") && required(password, "Password", "suPass")){
-		if (letAndNum(uName) && mvla(email)){
-			if (uName.length < 20 && uName.length > 6 && password.length > 8){
-				let thisUser = db.ref('users/' + uName);
+const db = firebase.database();
+var user = firebase.auth().currentUser;
+var name, email, photoUrl, uid, emailVerified;
+if (user != null) {
+  	name = user.displayName;
+  	email = user.email;
+  	photoUrl = user.photoURL;
+  	emailVerified = user.emailVerified;
+  	uid = user.uid;
+  	document.getElementById("headerBtn").innerHTML = '<nav class="mdl-navigation"><a class="mdl-navigation__link" href="#">Hello' + email + '!</a></nav>';
+}
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+  .then(function() {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  })
+  .catch(function(error) {
+    console.log(error.code);
+    console.log(error.message);
+});
+function createUser(){
+	//let suuName = document.getElementById('suName').value;
+	let suemail = document.getElementById('suEmail').value;
+	let supassword = document.getElementById('suPass').value;
+	/*if (required(uName, "Username", "suName") && required(email, "Email", "suEmail") && required(password, "Password", "suPass")){
+		if (letAndNum(uName) &&mvla(email)){
+			if (uName.length < 20 && uName.length > 6 && password.length > 8){*/
+				/*let thisUser = db.ref('users/' + uName);
 				let realAcc = await thisUser.once('value');
 				if (realAcc.val() == null){
 					thisUser.set({
@@ -17,30 +39,54 @@ async function createUser(){
 					alert('Your submission was sent');	
 				} else {
 					alert('This username already exists')
-				}
-				/*firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-		        var errorCode = error.code;
-		        var errorMessage = error.message;
-		        if (errorCode == 'auth/weak-password') {
-		          alert('The password is too weak.');
-		        } else {
-		          alert(errorMessage);
-		        }
-		        console.log*/
-		    }
-		}
-	}
+				}*/
+	firebase.auth().createUserWithEmailAndPassword(suemail, supassword).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+    });
+    alert("Account Created")
 }
-async function logIn(){
-	let uName = document.getElementById('liName').value;
-	let password = document.getElementById('liPass').value;
-	let realPass = await db.ref('users/' + uName + '/password/').once('value');
-	console.log("REALPASS: ", realPass.val());
+function logIn(){
+	let liemail = document.getElementById('liEmail').value;
+	let lipassword = document.getElementById('liPass').value;
+	//let realPass = await db.ref('users/' + uName + '/password/').once('value');
+	firebase.auth().signInWithEmailAndPassword(liemail, lipassword).catch(function(error) {
+	  	console.log(error.code);
+	  	console.log(error.message);
+	});
+	var user = firebase.auth().currentUser;
+	var name, email, photoUrl, uid, emailVerified;
+	if (user != null) {
+	  	name = user.displayName;
+	  	email = user.email;
+	  	photoUrl = user.photoURL;
+	  	emailVerified = user.emailVerified;
+	  	uid = user.uid;
+	  	document.getElementById("headerBtn").innerHTML = '<nav class="mdl-navigation"><a class="mdl-navigation__link" href="#">Hello' + email + '!</a></nav>';
+	}
+	console.log(name + ", " + email + ", " + uid);
+	/*console.log("REALPASS: ", realPass.val());
 	console.log(password)
 	if (realPass.val() == password){
 		alert('Logged in as ' + uName);
 		window.location.replace("file:///C:/Users/Camden/Documents/GitHub/nou/src/homePage/home.html")
 	} else {
 		alert('Incorrect username or password');
+	}*/
+}
+function logOut(){
+	firebase.auth().signOut().then(function() {
+  		// Sign-out successful.
+	}).catch(function(error) {
+  		// An error happened.
+	});
+	var user = firebase.auth().currentUser;
+	if (user == null){
+		document.getElementById("headerBtn").innerHTML = "<a class='mdl-button mdl-js-button mdl-button--raised mdl-button--accent' href='#'>Sign Up</a> <a class='mdl-button mdl-js-button' href='#'>Log In</a>"
 	}
 }
