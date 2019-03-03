047f80ba6d1671357433f5825666c0d700652b61
CamdenSun <100024246@mvla.net>
2019-03-02 21:38:00 -0800

    morestuff

--- index.js
+++ index.js
@@ -26,7 +26,45 @@ $(document).ready(function(){
 				var img = $("<img>").attr("id", "iImg");
 				$("#message").append(disp, user, desc, price, img);
 			}
-			//I need to fix what is below
-
+			db.ref("items").once("value", function(snap){
+				var items = snap.val();
+				var arr = Object.keys(items);
+				for (let i = 0; i < arr.length; i++){
+					console.log(items[arr[i]].desc);
+				}
+				var count = 0;
+				for (let i = 0; i < arr.length; i++){
+					if (strOccur(search, items[arr[i]].display) != 0){
+						count++;
+						var disp = $("<h3></h3>").text(items[arr[i]].display).attr("id", "iName");
+						var user = $("<p></p>").text(items[arr[i]].username).attr("id", "iUser");
+						var desc = $("<p></p>").text(items[arr[i]].description).attr("id", "iDesc");
+						var price = $("<p></p>").text("$" + items[arr[i]].price).attr("id", "iPrice");
+						var img = $("<img>").attr("src", items[arr[i]].image).attr("id", "iImage");
+						$("#searchbardiv").append(disp, user, desc, price, img);
+					}
+				}
+				if (count == 0){
+					$("#iName").text("Item Not Found");
+				}
+			}
+			itemRef.once("value", function(snap){
+				item = snap.val();
+				if (item != null){
+					$("#iName").text(item.display);
+					$("#iUname").text("Owner: " + item.username);
+					$("#iDesc").text("Description: " + item.description);
+					$("#iPrice").text("Price: $" + item.price);
+					$("#iImg").attr("src", item.image).attr("style", "width: 50px");
+				} else {
+					alert("Item not found");
+					$("#iName").text("ITEM NOT FOUND");
+					$("#iDesc").text("If you\'re searching for something, it\'s not here");
+				}
+			}).catch(function(err){
+				console.log(err.code);
+				console.log(err.message);
+			});
+		}
 	});
 });
